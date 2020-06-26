3960108c3ffc3ffbbe0120ab4257d39d00e6bdb2
York Liu <york0414@gmail.com>
2020-06-27 01:20:01 +0800

    zerro commit

---
+++ play.py
@@ -0,0 +1,31 @@
+price = '非常感謝'
+pen = '在分鐘的時間內'
+rain = '選擇' 
+name = 'York' 
+print(price, pen, rain, name)
+
+
+
+print('非常討厭你的大價光，我他嗎')
+
+
+
+
+name = input('請問您大名:  ') 
+print('嗨,',name, '歡迎')
+
+
+print('請選')
+
+
+gen = input ('性別:')
+
+print(gen)
+
+
+marriage = input ('婚姻:')
+
+print(marriage)
+
+
+print('請再次確認: 姓名:  ',name,  '性別:  ',gen,'婚姻狀況: ',marriage)
